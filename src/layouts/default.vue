dd668abf42ac90d4ed680323a95b4edb96e4fa8f
Joon Lee <l33j00n325@gmail.com>
2021-08-06 14:54:33 -0700

    add separate LoginPage

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,32 +1,36 @@
 <template>
-  <template v-if="token">
-    <h1>MyFIFA Manager</h1>
-    <main>
-      <router-view />
-    </main>
-    <button @click="logout">Log Out</button>
-  </template>
-  <template v-else>
-    <main>
-      <h1>MyFIFA Manager</h1>
-      <login-form />
-    </main>
-  </template>
+  <h1>MyFIFA Manager</h1>
+  <main>
+    <router-view />
+  </main>
+  <button
+    v-if="token"
+    @click="logout"
+  >
+    Log Out
+  </button>
 </template>
 
 <script setup>
-  import { computed } from 'vue'
+  import { computed, watch } from 'vue'
   import { useRouter } from 'vue-router'
   import { useMutation } from '@urql/vue'
   import gql from 'graphql-tag'
   import { useAuthStore } from '~/store/auth'
-  import LoginForm from '~/components/App/LoginForm.vue'
 
   const authStore = useAuthStore()
   const token = computed(() => authStore.token)
 
   const router = useRouter()
-  token.value && router.push('/teams')
+  function redirectIfUnauthenticated () {
+    if (token.value) {
+      router.push('/')
+    } else {
+      router.push('/login')
+    }
+  }
+  redirectIfUnauthenticated()
+  watch(token, redirectIfUnauthenticated)
 
   const { executeMutation: revokeAccessToken } = useMutation(gql`
     mutation revokeAccessToken($token: String!) {
@@ -35,7 +39,6 @@
       }
     }
   `)
-
   async function logout () {
     const { data: { revokeAccessToken: { errors} } } =
       await revokeAccessToken({ token: token.value })
