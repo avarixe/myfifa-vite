c7dd2d809fd3dd643e91a092baa407ddbcc064e2
Joon Lee <l33j00n325@gmail.com>
2022-07-14 09:10:17 -0700

    bump vite to 3.0.0

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 4000
+  },
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
