dad2ad703b432b63126893040b1b99cff935c862
Joon Lee <l33j00n325@gmail.com>
2023-03-09 22:10:17 +0000

    add submit type to button in MenuForm

--- src/components/Base/MenuForm.vue
+++ src/components/Base/MenuForm.vue
@@ -48,57 +48,57 @@
     persistent
     activator="parent"
   >
-    <v-card width="400px">
-      <v-toolbar dense :extended="formError.length > 0">
-        <slot name="header">
-          <v-toolbar-title>
-            <v-icon v-if="titleIcon" start>{{ titleIcon }}</v-icon>
-            {{ title }}
-          </v-toolbar-title>
-        </slot>
-        <template v-if="formError.length > 0" #extension>
-          <v-alert
-            type="error"
-            :text="formError"
-            :rounded="0"
-            density="compact"
-            closable
-            @update:model-value="dismissError"
-          />
-        </template>
-      </v-toolbar>
-      <v-divider />
-      <v-card-text :style="{ maxHeight: '50vh', overflowY: 'auto' }">
-        <v-form
-          ref="form"
-          :key="formKey"
-          v-model="formIsValid"
-          @submit.prevent="submitForm"
-        >
+    <v-form
+      ref="form"
+      :key="formKey"
+      v-model="formIsValid"
+      @submit.prevent="submitForm"
+    >
+      <v-card width="400px">
+        <v-toolbar dense :extended="formError.length > 0">
+          <slot name="header">
+            <v-toolbar-title>
+              <v-icon v-if="titleIcon" start>{{ titleIcon }}</v-icon>
+              {{ title }}
+            </v-toolbar-title>
+          </slot>
+          <template v-if="formError.length > 0" #extension>
+            <v-alert
+              type="error"
+              :text="formError"
+              :rounded="0"
+              density="compact"
+              closable
+              @update:model-value="dismissError"
+            />
+          </template>
+        </v-toolbar>
+        <v-divider />
+        <v-card-text :style="{ maxHeight: '50vh', overflowY: 'auto' }">
           <v-container>
             <v-row dense>
               <slot name="form" />
             </v-row>
           </v-container>
-        </v-form>
-      </v-card-text>
-      <v-divider />
-      <v-card-actions>
-        <v-spacer />
-        <v-btn size="large" :disabled="formIsLoading" @click="menu = false">
-          Cancel
-        </v-btn>
-        <slot name="additional-actions" />
-        <v-btn
-          :disabled="!formIsValid"
-          color="primary"
-          size="large"
-          :loading="formIsLoading"
-          @click="submitForm"
-        >
-          Save
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+        </v-card-text>
+        <v-divider />
+        <v-card-actions>
+          <v-spacer />
+          <v-btn size="large" :disabled="formIsLoading" @click="menu = false">
+            Cancel
+          </v-btn>
+          <slot name="additional-actions" />
+          <v-btn
+            type="submit"
+            :disabled="!formIsValid"
+            color="primary"
+            size="large"
+            :loading="formIsLoading"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
   </v-menu>
 </template>
