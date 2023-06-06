ae3d20415b073cde4625c74616f483f5d57ea3a5
Joon Lee <l33j00n325@gmail.com>
2023-06-06 02:36:45 +0000

    bump version to 3.0.1

--- src/components/App/AppEntryHeader.vue
+++ src/components/App/AppEntryHeader.vue
@@ -5,5 +5,5 @@
 <template>
   <v-img :src="logo" height="128px" />
   <div class="text-h3">MyFIFA Manager</div>
-  <div class="text-body-2">v3.0.0</div>
+  <div class="text-body-2">v3.0.1</div>
 </template>
