b4d92aa866e95c1173d058fdca95a08292b06754
Joon Lee <l33j00n325@gmail.com>
2023-06-12 21:49:16 +0000

    fix AppBreadcrumbs color

--- src/components/App/AppBreadcrumbs.vue
+++ src/components/App/AppBreadcrumbs.vue
@@ -10,14 +10,12 @@
   const { seasonLabel } = useTeam()
   const breadcrumbs = computed(() => {
     const routeSteps = route.name === '/' ? [''] : route.path.split('/')
-    console.log(route)
-    console.log(routeSteps)
     return routeSteps.map((step, i) => {
       const to = `${routeSteps.slice(0, i + 1).join('/')}`
       const prevStep = routeSteps[i - 1]
       switch (step) {
         case '': // root
-          return { to: '/', title: 'Home' }
+          return { to: '/', title: 'Home', exact: true }
         case 'teams':
         case 'players':
         case 'matches':
@@ -26,46 +24,53 @@
         case 'account':
         case 'development':
         case 'statistics':
-          return { to, title: capitalize(step) }
+          return { to, title: capitalize(step), exact: true }
         case 'seasons':
           return {
             to: to.replace(/seasons/, 'competitions'),
-            title: capitalize(step)
+            title: capitalize(step),
+            exact: true
           }
         case 'new':
           return {
             to,
-            title: `New ${capitalize(prevStep).replace(/e?s$/, '')}`
+            title: `New ${capitalize(prevStep).replace(/e?s$/, '')}`,
+            exact: true
           }
         case 'edit': {
           const twoStepsAgo = routeSteps[i - 2]
           return {
             to,
-            title: `Edit ${capitalize(twoStepsAgo).replace(/e?s$/, '')}`
+            title: `Edit ${capitalize(twoStepsAgo).replace(/e?s$/, '')}`,
+            exact: true
           }
         }
         default:
           switch (prevStep) {
             case 'teams': {
               const team = teamRepo.find(parseInt(step))
-              return { to, title: team?.name }
+              return { to, title: team?.name, exact: true }
             }
             case 'players': {
               const player = playerRepo.find(parseInt(step))
-              return { to, title: player?.name }
+              return { to, title: player?.name, exact: true }
             }
             case 'matches': {
               const match = matchRepo.find(parseInt(step))
-              return { to, title: `${match?.home} v ${match?.away}` }
+              return {
+                to,
+                title: `${match?.home} v ${match?.away}`,
+                exact: true
+              }
             }
             case 'competitions': {
               const competition = competitionRepo.find(parseInt(step))
-              return { to, title: competition?.name }
+              return { to, title: competition?.name, exact: true }
             }
             case 'seasons':
-              return { to, title: seasonLabel(parseInt(step)) }
+              return { to, title: seasonLabel(parseInt(step)), exact: true }
             default:
-              return { to, title: step }
+              return { to, title: step, exact: true }
           }
       }
     })
