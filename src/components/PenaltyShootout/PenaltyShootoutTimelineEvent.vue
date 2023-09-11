1d5002b506693f11d30082ca35552be145181946
Joon Lee <l33j00n325@gmail.com>
2023-09-10 17:13:28 -0700

    fix(PenaltyShootoutTimelineEvent): set side to winner
    resolves #63

--- src/components/PenaltyShootout/PenaltyShootoutTimelineEvent.vue
+++ src/components/PenaltyShootout/PenaltyShootoutTimelineEvent.vue
@@ -7,10 +7,17 @@
     readonly?: boolean
   }>()
 
+  const winnerIsHome = computed(
+    () => props.event.homeScore > props.event.awayScore
+  )
+
   const winner = computed(() =>
-    props.event.homeScore > props.event.awayScore
-      ? props.match.home
-      : props.match.away
+    winnerIsHome.value ? props.match.home : props.match.away
+  )
+
+  const { mdAndUp } = useDisplay()
+  const slotName = computed(() =>
+    winnerIsHome.value && mdAndUp.value ? 'opposite' : 'default'
   )
 </script>
 
@@ -19,30 +26,32 @@
     <template #icon>
       <small>{{ match.extraTime ? 120 : 90 }}'</small>
     </template>
-    <h2 class="font-weight-light my-0">
-      <span class="text-caption text-truncate mx-1 text-indigo">
-        Penalty Shootout
-      </span>
-    </h2>
-
-    <div :class="{ 'font-weight-bold': match.home === winner }">
-      {{ event.homeScore }} - {{ match.home }}
-    </div>
-    <div :class="{ 'font-weight-bold': match.away === winner }">
-      {{ event.awayScore }} - {{ match.away }}
-    </div>
-
-    <div v-if="!props.readonly">
-      <v-btn variant="text" color="orange">
-        Edit
-        <penalty-shootout-form :match="match" />
-      </v-btn>
-      <remove-button
-        :record="event"
-        store="PenaltyShootout"
-        label="Penalty Shootout"
-        variant="text"
+    <template #[slotName]>
+      <h2
+        v-if="!mdAndUp"
+        class="font-weight-light text-caption text-truncate mx-1 text-indigo my-0"
+        v-text="'Penalty Shootout'"
       />
-    </div>
+
+      <div :class="{ 'font-weight-bold': match.home === winner }">
+        {{ event.homeScore }} - {{ match.home }}
+      </div>
+      <div :class="{ 'font-weight-bold': match.away === winner }">
+        {{ event.awayScore }} - {{ match.away }}
+      </div>
+
+      <div v-if="!props.readonly">
+        <v-btn variant="text" color="orange">
+          Edit
+          <penalty-shootout-form :match="match" />
+        </v-btn>
+        <remove-button
+          :record="event"
+          store="PenaltyShootout"
+          label="Penalty Shootout"
+          variant="text"
+        />
+      </div>
+    </template>
   </v-timeline-item>
 </template>
