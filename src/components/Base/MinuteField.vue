a72148746872ec427746d7f196b9a959ac8272d6
Joon Lee <l33j00n325@gmail.com>
2023-11-16 06:49:59 +0000

    revert(MinuteField): change from VSelect to VSlider with incrementer buttons

--- src/components/Base/MinuteField.vue
+++ src/components/Base/MinuteField.vue
@@ -1,20 +1,46 @@
 <script setup lang="ts">
   import { Match } from '~/models'
 
-  defineProps<{
+  const props = defineProps<{
     modelValue: number | null
     match: Match
   }>()
 
   const emit = defineEmits(['update:modelValue'])
+
+  onMounted(() => {
+    props.modelValue || emit('update:modelValue', 1)
+  })
+
+  function decrement() {
+    emit(
+      'update:modelValue',
+      props.modelValue && props.modelValue > 1 ? props.modelValue - 1 : 1
+    )
+  }
+
+  function increment() {
+    emit(
+      'update:modelValue',
+      Number(props.modelValue) < props.match.endOfMatch
+        ? Number(props.modelValue) + 1
+        : props.match.endOfMatch
+    )
+  }
 </script>
 
 <template>
-  <v-select
-    label="Minute"
-    prepend-icon="mdi-timer"
-    :items="Array.from({ length: match.endOfMatch }, (_, i) => i + 1)"
-    :model-value="modelValue"
+  <div class="text-caption text-medium-emphasis">Minute</div>
+  <v-slider
+    :label="`${modelValue ?? 0}'`"
+    step="1"
+    :min="1"
+    :max="match.endOfMatch"
+    append-icon="mdi-plus"
+    prepend-icon="mdi-minus"
+    :model-value="modelValue ?? undefined"
     @update:model-value="emit('update:modelValue', $event)"
+    @click:append="increment"
+    @click:prepend="decrement"
   />
 </template>
